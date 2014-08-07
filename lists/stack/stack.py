456cbfc27f044e8851e22dc5a619175be06d1b25
Anthony Erlinger <aerlinger@gmail.com>
2014-08-06 22:11:04 -0400

    Add tests for Stack

--- lists/stack/stack.py
+++ lists/stack/stack.py
@@ -1,13 +1,17 @@
 class Stack:
+  __MAX__ = 10
+
   def __init__(self, items=[]):
     self.items = items
 
   def push(self, item):
+    if len(self.items) > Stack.__MAX__:
+      raise "Stack Overflow!"
     return self.items.append(item)
 
   def pop(self):
     if len(self.items) < 1:
-      raise "Fail!"
+      raise "Stack Underflow!"
 
     return self.items.pop()
 
--- test/lists/stack_test.py
+++ test/lists/stack_test.py
@@ -7,13 +7,18 @@ class StackTest(unittest.TestCase):
   def setUp(self):
     self.stack = Stack()
 
-  def test_push(self):
-    self.stack.push(5)
-
   def test_pop(self):
     self.stack.push(5)
     result = self.stack.pop()
+
     self.assertEquals(5, result)
 
   def test_pop_failure(self):
     self.assertRaises(Exception, self.stack.pop)
+
+  def test_push_failure(self):
+    for i in range(11):
+      self.stack.push(i)
+
+    self.assertRaises(Exception, self.stack.push, 5)
+    
